cc666e7d0207cbac702db946a891bff2a9e455a0
Prakhar Bhatia <prakhar.bhatia@nagarro.com>
2020-03-31 17:14:29 +0530

    my project

---
+++ src/main/java/com/prakhar/config/SpringWebConfiguration.java
@@ -0,0 +1,21 @@
+package com.prakhar.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.ViewResolver;
+
+@Configuration
+@ComponentScan(basePackages="com.prakhar")
+@EnableWebMvc
+
+public class SpringWebConfiguration {
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver("/jsps/", ".jsp");
+		return viewResolver;
+	}
+
+}
